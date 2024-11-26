e58fccf9b8e5d00f9bf1ea39fb80b7007816aafa
nicolas <nicolas.rodriguez@1nce.com>
2024-11-26 19:31:10 -0300

    Fixed invalid tokne validation.

--- ReactNative/utils/tokenHandler.js
+++ ReactNative/utils/tokenHandler.js
@@ -19,19 +19,3 @@ export async function deleteToken() {
     return null;
   }
 }
-
-export async function validateToken() {
-  const navigation = useNavigation();
-
-  try {
-    const token = await getToken();
-    if (!token) {
-      console.log("Token does not exist. Redirecting to (tabs)...");
-      navigation.replace("(tabs)");
-    } else {
-      console.log("Token exists:", token);
-    }
-  } catch (error) {
-    console.error("Error validating token:", error);
-  }
-}
